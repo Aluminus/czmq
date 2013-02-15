75b5f1ddaaf2458d8bb800fe61c800868de6d1ce
Matthew Plymale <matt.plyman@gmail.com>
2013-02-15 16:24:49 -0500

    Do not redefine C++ keywords

--- include/czmq_prelude.h
+++ include/czmq_prelude.h
@@ -380,10 +380,11 @@ typedef unsigned int    qbyte;          //  Quad byte = 32 bits
 #   define randof(num)  (int) ((float) (num) * random () / (RAND_MAX + 1.0))
 #endif
 //  I'd rather use stdbool.h but that is not available on current MSVC
-#if (!defined (true))
-#    define true        1
-#    define false       0
-     typedef char bool;
+#if (!defined (__cplusplus))	// It is illegal to redefine keywords in C++
+#   if (!defined (true))
+#       define true        1
+#       define false       0
+#   endif
 #endif
 //  Deprecated, remove at some stage
 #if (!defined (TRUE))
@@ -394,7 +395,9 @@ typedef unsigned int    qbyte;          //  Quad byte = 32 bits
 //- A number of POSIX and C99 keywords and data types -----------------------
 
 #if (defined (__WINDOWS__))
-#   define inline __inline
+#   if (!defined (__cplusplus))
+#      define inline __inline
+#   endif
 #   define strtoull _strtoui64
 #   define srandom srand
 #   define TIMEZONE _timezone
